42829089d2de841ac12dfe6722a2b01cf7ebae48
yoonbumtae <yoonbumtae@gmail.com>
2020-07-17 17:35:59 +0900

    Initial commit

---
+++ src/test/java/com/example/deretopic/DeretopicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deretopic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeretopicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
