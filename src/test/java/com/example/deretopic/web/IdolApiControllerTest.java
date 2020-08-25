a220dd75918d48f7ab8f2f177540beefe0c8c95b
yoonbumtae <yoonbumtae@gmail.com>
2020-08-25 16:55:14 +0900

    many to one #4

--- src/test/java/com/example/deretopic/web/IdolApiControllerTest.java
+++ src/test/java/com/example/deretopic/web/IdolApiControllerTest.java
@@ -1,5 +1,11 @@
 package com.example.deretopic.web;
 
+import com.example.deretopic.domain.common.BloodType;
+import com.example.deretopic.domain.common.Constellation;
+import com.example.deretopic.domain.common.IdolType;
+import com.example.deretopic.domain.common.ThreeSize;
+import com.example.deretopic.domain.idol.IdolEntity;
+import com.example.deretopic.domain.idol.IdolEntityRepository;
 import com.example.deretopic.domain.uwasa.UwasaEntity;
 import com.example.deretopic.domain.uwasa.UwasaEntityRepository;
 import com.example.deretopic.web.dto.UwasaEntitySaveDTO;
@@ -19,6 +25,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -38,6 +45,9 @@ public class IdolApiControllerTest {
 
     @Autowired
     private UwasaEntityRepository uwasaEntityRepository;
+
+    @Autowired
+    private IdolEntityRepository idolEntityRepository;
 
     private MockMvc mvc;
 
@@ -59,6 +69,33 @@ public class IdolApiControllerTest {
     @WithMockUser(roles = "USER")
     public void uwasa_enroll() throws Exception {
         // given
+        String name = "アナスタシア";
+        Integer age = 15;
+        LocalDate birthday = LocalDate.of(0, 9, 19);
+        String birthPlace = "北海道";
+        BloodType bloodType = BloodType.O;
+        Integer height = 165;
+        Integer weight = 43;
+        ThreeSize threeSize = ThreeSize.builder().bust(80).waist(54).hip(80).build();
+        Constellation constellation = Constellation.VIRGO;
+        String hobby = "ホームパーティ,天体観測";
+        IdolType idolType = IdolType.COOL;
+        IdolEntity idolEntity = IdolEntity.builder()
+                .name(name)
+                .age(age)
+                .birthday(birthday)
+                .birthPlace(birthPlace)
+                .bloodType(bloodType)
+                .height(height)
+                .weight(weight)
+                .threeSize(threeSize)
+                .constellation(constellation)
+                .hobby(hobby)
+                .idolType(idolType)
+                .build();
+        idolEntityRepository.save(idolEntity);
+
+        // uwasa given
         String idolNameJa = "ja";
         Integer topicNum = 1;
         String uwasaJa = "ee";
