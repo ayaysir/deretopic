98983f3e6435c1b7079206b5c5c3bdca6e008161
yoonbumtae <yoonbumtae@gmail.com>
2020-08-29 20:41:45 +0900

    spa oauth2 연동 #1

--- src/main/java/com/example/deretopic/DeretopicApplication.java
+++ src/main/java/com/example/deretopic/DeretopicApplication.java
@@ -1,9 +1,7 @@
 package com.example.deretopic;
 
-import com.example.deretopic.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 public class DeretopicApplication {
